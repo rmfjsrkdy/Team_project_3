7c91e9bbb99668b8def7777ef4d5efd8fa06082c
rmfjsrkdy <injin032@gmail.com>
2025-12-03 06:17:46 +0000

    fix: API 키 완전 제거

--- pages/2_🔧_유지보수_전문가.py
+++ pages/2_🔧_유지보수_전문가.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 
 st.title("🔧 집수리 해결사 챗봇")
 st.text("수리 및 유지보수가 필요하다면 언제든지 물어보세요!")
-client = OpenAI(api_key = "sk")
+client = OpenAI(api_key = "")
 
 if "chatbot_messages" not in st.session_state:
     st.session_state.chatbot_messages = [
