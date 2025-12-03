f322e3a7b1c845345962948265211501ebf2055a
rmfjsrkdy <injin032@gmail.com>
2025-12-03 06:09:19 +0000

    repaor code

--- pages/2_🔧_유지보수_전문가.py
+++ pages/2_🔧_유지보수_전문가.py
@@ -1,1 +1,34 @@
 import streamlit as st
+from openai import OpenAI
+
+st.title("🔧 집수리 해결사 챗봇")
+st.text("수리 및 유지보수가 필요하다면 언제든지 물어보세요!")
+client = OpenAI(api_key = "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")
+
+if "chatbot_messages" not in st.session_state:
+    st.session_state.chatbot_messages = [
+        {"role":"system","content":f"""당신은 생활 고장 수리 및 유지보수를 잘하는 AI 해결사 입니다. 사용자의 문제를 응답받으면 해결방안을 간단하게 답변 하시오.
+"""}
+    ]
+
+def show_message(msg):
+    with st.chat_message(msg["role"]):
+        st.markdown(msg["content"])
+
+for msg in st.session_state.chatbot_messages[1:]:
+    show_message(msg)
+
+if prompt := st.chat_input("수리가 필요한 상황 입력하기"):
+    msg = {"role":"user", "content":prompt}
+    show_message(msg)
+    st.session_state.chatbot_messages.append(msg)
+
+    response = client.chat.completions.create(
+        model = "gpt-5-mini",
+        messages = st.session_state.chatbot_messages
+    )
+    msg = {"role":"assistant", "content":response.choices[0].message.content}
+    show_message(msg)
+    st.session_state.chatbot_messages.append(msg)
+
+    st.session_state.chatbot_messages.append(assistant_msg)
